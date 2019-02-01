9a94f16a9a05ea759dec87e4cd574b8b7c77888f
Alice Liang <alicel_@alices-macbook-air.local>
2019-01-31 22:27:28 -0500

    3.2 completed

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessedLetterButton: UIButton!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        print("In viewDidLoad, is guessLetterField the first responder?", guessedLetterField.isFirstResponder)
     }
-
-
+    
+    func updateUIAfterGuess(){
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+    }
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+//        print("Hey, The guessedLetterFieldChanged!")
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+//                print("In doneKeyPressed, is guessLetterField the first responder before updateUIGuessed?", guessedLetterField.isFirstResponder)
+        updateUIAfterGuess()
+//        print("In doneKeyPressed, is guessLetterField the first responder after updateUIGuessed?", guessedLetterField.isFirstResponder)
+    }
+    
+    @IBAction func guessedLetterButtonPressed(_ sender: UIButton) {
+//        print("In guessedLetterButtonPressed, is guessLetterField the first responder before updateUIGuessed?", guessedLetterField.isFirstResponder)
+        updateUIAfterGuess()
+//        print("In guessedLetterButtonPressed, is guessLetterField the first responder after updateUIGuessed?", guessedLetterField.isFirstResponder)
+        
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
